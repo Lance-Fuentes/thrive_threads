33009b3d47ba92a9dbca87f984fe9283781cb3ba
Lance-Fuentes <lfuentes.tech@gmail.com>
2024-04-05 00:59:19 -0500

    Generate models

---
+++ db/migrate/20240405055901_create_orders.rb
@@ -0,0 +1,13 @@
+class CreateOrders < ActiveRecord::Migration[7.1]
+  def change
+    create_table :orders do |t|
+      t.float :total
+      t.string :description
+      t.string :status
+      t.datetime :created_at
+      t.references :user, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
