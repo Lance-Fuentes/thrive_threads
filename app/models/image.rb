1df7798c1fbb9dfda46790f03a3b5788ba41ced3
Lance-Fuentes <lfuentes.tech@gmail.com>
2024-04-05 02:33:11 -0500

    Added crud to admin, and updated ransack permissions

--- app/models/image.rb
+++ app/models/image.rb
@@ -1,3 +1,7 @@
 class Image < ApplicationRecord
   belongs_to :product
+
+  def self.ransackable_attributes(auth_object = nil)
+    ["created_at", "id", "id_value", "product_id", "updated_at"]
+  end
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,13 @@
 class Product < ApplicationRecord
   belongs_to :category
+  has_many :images
+  validates :name, :price, :description, presence: true
+
+  def self.ransackable_attributes(auth_object = nil)
+    ["category_id", "created_at", "description", "id", "name", "price", "updated_at"]
+  end
+
+  def self.ransackable_associations(auth_object = nil)
+    ["category", "images"]
+  end
 end
