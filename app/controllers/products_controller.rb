aac74f1c3d65caf004e4d6de5a35ed6cc2426a4a
Lance-Fuentes <lfuentes.tech@gmail.com>
2024-04-12 05:48:23 -0500

    readability line spaces

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -4,21 +4,31 @@ class ProductsController < InheritedResources::Base
     @products = Product.all
     add_breadcrumb "Home", :root_path
 
-    if params[:category_id]
+    if
+      params[:category_id]
       @category = Category.find(params[:category_id])
       @products = @category.products
       add_breadcrumb "Categories", :categories_path
       add_breadcrumb @category.name, category_path(@category)
       add_breadcrumb "Products", category_products_path(@category)
-    elsif params[:sale] == 'true'
+
+    elsif
+      params[:sale] == 'true'
       @products = @products.on_sale
       add_breadcrumb 'On Sale Products', products_path(sale: true)
-    elsif params[:new] == 'true'
-      @products = Product.where('created_at >= ?', 60.minutes.ago)
+
+    elsif
+      params[:new] == 'true'
+      @products = Product.where('created_at >= ?', 3.days.ago)
       add_breadcrumb 'New Products', products_path(new: true)
-    elsif params[:updated] == 'true'
-      @products = Product.where('updated_at >= ?', 60.minutes.ago)
+
+    elsif
+      params[:updated] == 'true'
+      updated_products = @products.where('updated_at >= ?', 3.days.ago)
+                                  .where.not('created_at >= ?', 3.days.ago)
+      @products = updated_products
       add_breadcrumb 'Recently Updated Products', products_path(updated: true)
+
     else
       add_breadcrumb "All Products", products_path
     end
