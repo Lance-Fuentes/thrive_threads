2fa7d391f3732648de63ebcb51046f1aede28de0
Lance-Fuentes <lfuentes.tech@gmail.com>
2024-04-12 04:34:26 -0500

    added functions for admin product form, updated category id to show names instead

--- app/admin/products.rb
+++ app/admin/products.rb
@@ -20,9 +20,42 @@ ActiveAdmin.register Product do
     f.semantic_errors
     f.inputs
     f.inputs do
-      f.input :images, as: :file, input_html: { multiple: true }, hint: f.object.images.present? ? f.object.images.map { |image| image_tag(image) }.join.html_safe : content_tag(:span, "")
+      f.has_many :images, allow_destroy: true, heading: 'Images', new_record: 'Add Image' do |i|
+        if i.object.file.attached?
+          i.input :file, as: :file, label: 'Image',
+                             hint: image_tag(url_for(i.object.file), height: '100')
+        else
+          i.input :file, as: :file, label: 'Image'
+        end
+      end
     end
+
     f.actions
+  end
+
+
+  controller do
+    def create
+      @product = Product.new(product_params)
+      if @product.save
+        redirect_to admin_product_path(@product), notice: 'Product was successfully created.'
+      else
+        render :new
+      end
+    end
+
+    def update
+      @product = Product.find(params[:id])
+      if @product.update(product_params)
+        redirect_to admin_product_path(@product), notice: 'Product was successfully updated.'
+      else
+        render :edit
+      end
+    end
+
+    def product_params
+      params.require(:product).permit(:name, :price, :description, :category_id, images_attributes: [:id, :file, :_destroy])
+    end
   end
 
   show do
@@ -30,11 +63,13 @@ ActiveAdmin.register Product do
       row :name
       row :price
       row :description
-      row :category_id
+      row :category do |product|
+        product.category.name if product.category
+      end
       row :images do |product|
         product.images.each do |image|
           span do
-            image_tag url_for(image.file)
+            image_tag(url_for(image.file), height: '500')
           end
         end
       end
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,6 +1,7 @@
 class Product < ApplicationRecord
   belongs_to :category
   has_many :images, dependent: :destroy
+  accepts_nested_attributes_for :images, allow_destroy: true
   validates :name, :price, :description, :category_id, presence: true
 
   def self.ransackable_attributes(auth_object = nil)
